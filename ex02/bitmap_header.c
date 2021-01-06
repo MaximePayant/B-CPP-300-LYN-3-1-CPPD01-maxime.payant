4e349401048e9d039d0e1f2bb8c3ad80b185f117
Maxime Payant <maxime@pop-os.localdomain>
2021-01-06 13:38:30 +0100

    bmp

---
+++ ex02/bitmap_header.c
@@ -0,0 +1,31 @@
+/*
+** C++ Seminar Day01, 2020
+** Ex02 bitmap
+** bitmap_header.c
+*/
+
+#include "bitmap.h"
+
+void make_bmp_header(bmp_header_t *header, size_t size)
+{
+    header->magic = 16973;
+    header->size = (size * size * 4) + sizeof(bmp_info_header_t) + sizeof(bmp_header_t);
+    header->_app1 = 0;
+    header->_app2 = 0;
+    header->offset = 0;
+}
+
+void make_bmp_info_header(bmp_info_header_t *header, size_t size)
+{
+    header->size = 40;
+    header->width = size;
+    header->height = size;
+    header->planes = 1;
+    header->bpp = 32;
+    header->compression = 0;
+    header->raw_data_size = size * size * 4;
+    header->h_resolution = 0;
+    header->v_resolution = 0;
+    header->palette_size = 0;
+    header->important_colors = 0;
+}

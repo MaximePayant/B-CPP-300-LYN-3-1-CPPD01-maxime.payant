b8ae095cc5fa80519978755a05b9469b849324dc
Maxime Payant <maxime@pop-os.localdomain>
2021-01-06 20:12:08 +0100

    FUCK

---
+++ ex03/pyramid.c
@@ -0,0 +1,18 @@
+/*
+** C++ Seminar Day01, 2020
+** Ex03 pyramid
+** pyramid.c
+*/
+
+int pyramid_path(int size, const int **map)
+{
+    int path[size];
+
+    for (int lvl = 0; lvl < size; lvl++)
+        path[lvl] = map[size - 1][lvl];
+    for (int lvl = size - 2; lvl >= 0; lvl -= 1)
+        for (int room = 0; room <= lvl; room += 1)
+            path[room] = (path[room] < path[room + 1] ?
+            map[lvl][room] + path[room] : map[lvl][room] + path[room + 1]);
+    return (path[0]);
+}
